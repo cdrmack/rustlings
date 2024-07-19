a54a44a8e4645eff81e5c24674e53dc000c3ce04
cdrmack <git.pynvt@passmail.net>
2024-07-19 14:03:19 +0200

    improve hashmaps3 solution

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -31,19 +31,27 @@ fn build_scores_table(results: &str) -> HashMap<&str, Team> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
-        let team = scores.entry(team_1_name).or_insert(Team {
-            goals_scored: 0,
-            goals_conceded: 0,
-        });
-        team.goals_scored += team_1_score;
-        team.goals_conceded += team_2_score;
+        scores
+            .entry(team_1_name)
+            .and_modify(|t: &mut Team| {
+                t.goals_scored += team_1_score;
+                t.goals_conceded += team_2_score;
+            })
+            .or_insert(Team {
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
 
-        let team = scores.entry(team_2_name).or_insert(Team {
-            goals_scored: 0,
-            goals_conceded: 0,
-        });
-        team.goals_scored += team_2_score;
-        team.goals_conceded += team_1_score;
+        scores
+            .entry(team_2_name)
+            .and_modify(|t: &mut Team| {
+                t.goals_scored += team_2_score;
+                t.goals_conceded += team_1_score;
+            })
+            .or_insert(Team {
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            });
     }
 
     scores

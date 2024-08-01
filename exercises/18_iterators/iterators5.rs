dbf78239a7475ef2c39d3cc069a7b5ed70805730
cdrmack <git.pynvt@passmail.net>
2024-08-01 18:10:10 +0200

    improve iterators5
    replace iter with values inside count_iterator

--- exercises/18_iterators/iterators5.rs
+++ exercises/18_iterators/iterators5.rs
@@ -28,7 +28,7 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // `map` is a hash map with `String` keys and `Progress` values.
     // map = { "variables1": Complete, "from_str": None, … }
-    map.iter().filter(|&x| *x.1 == value).count()
+    map.values().filter(|&x| *x == value).count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
